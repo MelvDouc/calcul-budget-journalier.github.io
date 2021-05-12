4fb503009a166b50a9f67d91acc772096172f438
MelvDouc <melv.douc@gmail.com>
2021-05-12 23:21:53 +0200

    syntax

--- assets/js/classes/InputDiv.js
+++ assets/js/classes/InputDiv.js
@@ -1,30 +1,30 @@
 export class InputDiv {
-    constructor(id, title, labelTxt, min, max, val) {
-        this.id = id;
-        this.title = title;
-        this.labelTxt = labelTxt;
-        this.min = min;
-        this.max = max;
-        this.val = val;
+  constructor(id, title, labelTxt, min, max, val) {
+    this.id = id;
+    this.title = title;
+    this.labelTxt = labelTxt;
+    this.min = min;
+    this.max = max;
+    this.val = val;
 
-        let div = document.createElement('DIV')
-        div.classList.add('input');
+    const div = document.createElement("DIV");
+    div.classList.add("input");
 
-        let label = document.createElement('LABEL');
-        label.setAttribute('for', this.id);
-        label.setAttribute('title', this.title);
-        label.innerText = this.labelTxt;
+    const label = document.createElement("LABEL");
+    label.setAttribute("for", this.id);
+    label.setAttribute("title", this.title);
+    label.innerText = this.labelTxt;
 
-        let input = document.createElement('INPUT');
-        input.type = 'number';
-        input.name = this.id;
-        input.id = this.id;
-        input.min = this.min;
-        input.max = this.max;
-        input.setAttribute('value', this.val)
+    const input = document.createElement("INPUT");
+    input.type = "number";
+    input.name = this.id;
+    input.id = this.id;
+    input.min = this.min;
+    input.max = this.max;
+    input.setAttribute("value", this.val);
 
-        div.append(label, input);
+    div.append(label, input);
 
-        return div;
-    }
+    return div;
+  }
 }
